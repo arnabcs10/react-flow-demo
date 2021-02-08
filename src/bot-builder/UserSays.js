768317ad4ed86a7a3eb2e9c6ec38595ef3dd55f6
arnabcs10 <arnab10panda@gmail.com>
2021-02-08 16:32:39 +0530

    UserSays and userInput added

--- src/bot-builder/UserSays.js
+++ src/bot-builder/UserSays.js
@@ -1,10 +1,29 @@
+import React, {useEffect,useState} from 'react';
+import UserInput from './UserInput';
+let count = 1;
 const UserSays = () => {
-  const addTextArea = () => {
-    let container = document.querySelector(".sortable-objects");
-    let textField = document.querySelector("#us-1").cloneNode(true);
-    container.appendChild(textField);
-  };
-  // const deleteTextField = () => {};
+
+  const [userIntents, setUserIntents] = useState([]);
+
+  const addAnotherInput = ()=>{
+    ++count;
+    const newInput = {
+        id: count,
+        content:""
+    }
+    setUserIntents(userIntents => [...userIntents,newInput]);
+}
+
+  useEffect(() => {
+    setUserIntents([
+        {
+            id:count,
+            content:""
+           
+        }
+    ])
+    // eslint-disable-next-line
+}, []);
 
   return (
     <div className="tab">
@@ -47,55 +66,8 @@ const UserSays = () => {
       </label>
       <div className="tab-content">
         <div className="tr-blognav-container pa5 pt6 bg-grouped-table shadow-1 br3">
-          <form
-            id="settings-navigation"
-            className="tr-blognav"
-            noValidate="novalidate"
-          >
-            <div id="ember55" className="sortable-objects ember-view">
-              <div
-                draggable="false"
-                id="us-1"
-                className="js-draggableObject draggable-object ember-view"
-              >
-                <div
-                  id="ember57"
-                  className="tr-blognav-item tr-blognav-item--sortable ember-view"
-                >
-                  <div className="tr-blognav-line">
-                    <span id="ember60" className="tr-blognav-url ember-view">
-                      <input
-                        id="ember61"
-                        className="ember-text-field tr-input ember-view"
-                        type="text"
-                      />
-                      <p className="response" hidden=""></p>
-                    </span>
-                  </div>
-
-                  <button
-                    className="tr-blognav-delete"
-                    onClick={(e) => {
-                      document
-                        .querySelector(".tr-blognav-delete")
-                        .parentNode.parentNode.parentNode.removeChild(
-                          document.querySelector(".tr-blognav-delete")
-                            .parentNode.parentNode
-                        );
-                    }}
-                    type="button"
-                    data-ember-action=""
-                    data-ember-action-62="62"
-                  >
-                    <svg viewBox="0 0 32 32">
-                      <path d="M30.688 4H22V.687a.694.694 0 00-.688-.688H10.687a.694.694 0 00-.688.688V4H1.311c-.375 0-.625.313-.625.688s.25.625.625.625h3.375v26c0 .375.25.688.625.688h21.375c.375 0 .625-.313.625-.688v-26h3.375c.375 0 .625-.25.625-.625S31.061 4 30.686 4zM11.313 1.313h9.375v2.688h-9.375zM26 30.688H6V5.313h20zM10.688 9.313a.694.694 0 00-.688.688v15.313c0 .375.313.688.688.688s.625-.313.625-.688V10.001c0-.375-.25-.688-.625-.688zm5.312 0a.694.694 0 00-.688.688v15.313c0 .375.313.688.688.688s.688-.313.688-.688V10.001A.694.694 0 0016 9.313zm4.688.687v15.313c0 .375.25.688.625.688s.688-.313.688-.688V10c0-.375-.313-.688-.688-.688s-.625.313-.625.688z"></path>
-                    </svg>
-                    <span className="sr-only">Delete</span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </form>
+          {userIntents.map((intent, i) => <UserInput key={intent.id} intent={intent} setUserIntents={setUserIntents}/>)}
+          
           <button
             type="button"
             style={{
@@ -108,7 +80,7 @@ const UserSays = () => {
               padding: "5px",
             }}
             className="tr-btn"
-            onClick={addTextArea}
+            onClick={addAnotherInput}
           >
             + Add Another
           </button>
