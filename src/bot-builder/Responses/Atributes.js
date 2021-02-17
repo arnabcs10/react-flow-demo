f5e08fb7c31c78f1889b1d79698eeb5fbbe13766
arnabcs10 <arnab10panda@gmail.com>
2021-02-17 11:37:29 +0530

    modified attributes styles

--- src/bot-builder/Responses/Atributes.js
+++ src/bot-builder/Responses/Atributes.js
@@ -3,14 +3,16 @@ const Attributes = () => {
     <div>
       <input
         type="text"
-        style={{ width: "100px", display: "inline-block" }}
+        style={{ width: "47.5%", display: "inline-block" }}
         className="tr-input"
         placeholder="KEY"
       />
+      <div style={{display: "inline-block", margin: "10px", fontSize: "large", fontWeight: 700}}>
       :
+      </div>
       <input
         type="text"
-        style={{ width: "100px", display: "inline-block" }}
+        style={{ width: "47.5%", display: "inline-block" }}
         className="tr-input"
         placeholder="Value"
       />
